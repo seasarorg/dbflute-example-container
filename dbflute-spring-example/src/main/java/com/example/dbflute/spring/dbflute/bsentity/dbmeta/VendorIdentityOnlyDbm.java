737d8ec18fd2ef01db1e55b0661c3c110bffd43a
jflute <dbflute@gmail.com>
2014-05-05 02:51:54 +0900

    refactor for Scala

--- dbflute-spring-example/src/main/java/com/example/dbflute/spring/dbflute/bsentity/dbmeta/VendorIdentityOnlyDbm.java
+++ dbflute-spring-example/src/main/java/com/example/dbflute/spring/dbflute/bsentity/dbmeta/VendorIdentityOnlyDbm.java
@@ -73,7 +73,7 @@ public class VendorIdentityOnlyDbm extends AbstractDBMeta {
     // ===================================================================================
     //                                                                         Column Info
     //                                                                         ===========
-    protected final ColumnInfo _columnIdentityOnlyId = cci("IDENTITY_ONLY_ID", "IDENTITY_ONLY_ID", null, null, true, "identityOnlyId", Long.class, true, true, "BIGINT", 19, 0, "NEXT VALUE FOR PUBLIC.SYSTEM_SEQUENCE_3DE51E94_2BE0_44C2_AB8A_76E6709988CA", false, null, null, null, null, null);
+    protected final ColumnInfo _columnIdentityOnlyId = cci("IDENTITY_ONLY_ID", "IDENTITY_ONLY_ID", null, null, true, "identityOnlyId", Long.class, true, true, "BIGINT", 19, 0, "NEXT VALUE FOR PUBLIC.SYSTEM_SEQUENCE_3F30E60D_B529_4EE2_BD9E_6B7598DF16D2", false, null, null, null, null, null);
 
     public ColumnInfo columnIdentityOnlyId() { return _columnIdentityOnlyId; }
 
