9bb3ead94fac3e8d1b996a192457e2f0d1742b6e
jflute <dbflute@gmail.com>
2019-01-01 14:53:18 +0900

    add "export JAVA_HOME=$(/usr/libexec/java_home -v 1.6)" for mojave

--- dbflute-spring-example/dbflute_exampledb/_project.sh
+++ dbflute-spring-example/dbflute_exampledb/_project.sh
@@ -7,3 +7,7 @@ export DBFLUTE_HOME=../mydbflute/dbflute-1.x
 export MY_PROJECT_NAME=exampledb
 
 export MY_PROPERTIES_PATH=build.properties
+
+if [ `uname` = "Darwin" ]; then
+  export JAVA_HOME=$(/usr/libexec/java_home -v 1.6)
+fi
